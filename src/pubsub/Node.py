bb0b6260e333869ff610b47299c8d9b149dbadbf
essmoncif <moncefessaoudi@gmail.com>
2020-08-13 01:39:02 +0100

    add Node server

---
+++ src/pubsub/Node.py
@@ -0,0 +1,28 @@
+class Node:
+
+    def __init__(self):
+        
+        self._publishers = list()
+        self._subscribers = list()
+
+    def add_publisher(self, publisher):
+        
+        for pub in self._publishers:
+            if pub.addess == publisher.address:
+                raise Exception("This publisher is already exist")
+        self._publishers.append(publisher)
+    
+    def add_subscriber(self, subscriber):
+
+        for sub in self._subscribers:
+            if sub.address == subscriber.address:
+                raise Exception("This subscriber is already exist")
+        self._subscribers.append(subscriber)
+
+    def broadcast(self, publisher, message):
+        for sub in self._subscribers:
+            for follow in sub.follows:
+                if follow.address == publisher.address:
+                    sub.receive("{}! from {}".format(message, publisher.address))
+                    break
+    
