2cc75d22fd5aec4232d55e189e2f25a304aa961d
essmoncif <moncefessaoudi@gmail.com>
2020-08-15 19:07:46 +0100

    make node class as singleton

--- src/pubsub/Node.py
+++ src/pubsub/Node.py
@@ -1,21 +1,30 @@
 from src.blockchain_core.Blockchain import Blockchain
 
 
-class Node:
+class MetaClass(type):
+    _instance = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instance:
+            cls._instance[cls] = super(MetaClass, cls).__call__(*args, **kwargs)
+            return cls._instance[cls]
+
+
+class Node(metaclass=MetaClass):
 
     def __init__(self):
-        
+
         self._publishers = list()
         self._subscribers = list()
         self._blockchain = Blockchain(2, 3)
 
     def add_publisher(self, publisher):
-        
+
         for pub in self._publishers:
             if pub.addess == publisher.address:
                 raise Exception("This publisher is already exist")
         self._publishers.append(publisher)
-    
+
     def add_subscriber(self, subscriber):
 
         for sub in self._subscribers:
@@ -33,3 +42,6 @@ class Node:
 
 if __name__ == '__main__':
     node = Node()
+    print(node)
+    node1 =Node()
+    print(node1)
